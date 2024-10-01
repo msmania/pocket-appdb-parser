4ee7be583fe62b2380ea431162bc823ae099aba0
Toshihito Kikuchi <k.toshihito@yahoo.de>
2024-10-01 12:42:36 -0700

    More commands

--- go.mod
+++ go.mod
@@ -17,6 +17,7 @@ replace github.com/tendermint/tm-db => github.com/pokt-network/tm-db v0.5.2-0.20
 
 require (
 	github.com/ChainSafe/go-schnorrkel v0.0.0-20200405005733-88cbf1b4c40d // indirect
+	github.com/Workiva/go-datastructures v1.0.52 // indirect
 	github.com/beorn7/perks v1.0.1 // indirect
 	github.com/btcsuite/btcd v0.20.1-beta // indirect
 	github.com/cespare/xxhash/v2 v2.2.0 // indirect
@@ -31,19 +32,29 @@ require (
 	github.com/golang/snappy v0.0.4 // indirect
 	github.com/google/btree v1.0.0 // indirect
 	github.com/google/go-cmp v0.5.9 // indirect
+	github.com/gorilla/websocket v1.4.2 // indirect
 	github.com/gtank/merlin v0.1.1 // indirect
 	github.com/gtank/ristretto255 v0.1.2 // indirect
 	github.com/hashicorp/golang-lru v0.5.4 // indirect
+	github.com/inconshreveable/mousetrap v1.0.0 // indirect
 	github.com/jmhodges/levigo v1.0.0 // indirect
 	github.com/jordanorelli/lexnum v0.0.0-20141216151731-460eeb125754 // indirect
 	github.com/libp2p/go-buffer-pool v0.0.2 // indirect
 	github.com/matttproud/golang_protobuf_extensions v1.0.1 // indirect
 	github.com/mimoo/StrobeGo v0.0.0-20181016162300-f8f6d4d2b643 // indirect
+	github.com/minio/highwayhash v1.0.2 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/prometheus/client_golang v1.11.0 // indirect
 	github.com/prometheus/client_model v0.2.0 // indirect
 	github.com/prometheus/common v0.30.0 // indirect
 	github.com/prometheus/procfs v0.7.3 // indirect
+	github.com/rcrowley/go-metrics v0.0.0-20200313005456-10cdbea86bc0 // indirect
+	github.com/regen-network/cosmos-proto v0.3.0 // indirect
+	github.com/rs/cors v1.7.0 // indirect
 	github.com/spaolacci/murmur3 v1.1.0 // indirect
+	github.com/spf13/cobra v1.4.0 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
+	github.com/stretchr/testify v1.7.0 // indirect
 	github.com/tecbot/gorocksdb v0.0.0-20191217155057-f0fad39f321c // indirect
 	github.com/tendermint/tendermint v0.33.7 // indirect
 	github.com/tendermint/tm-db v0.5.1 // indirect
@@ -54,9 +65,11 @@ require (
 	golang.org/x/exp v0.0.0-20230131160201-f062dba9d201 // indirect
 	golang.org/x/net v0.9.0 // indirect
 	golang.org/x/sys v0.14.0 // indirect
+	golang.org/x/term v0.7.0 // indirect
 	golang.org/x/text v0.9.0 // indirect
 	google.golang.org/genproto v0.0.0-20230306155012-7f2fa6fef1f4 // indirect
 	google.golang.org/grpc v1.55.0 // indirect
 	google.golang.org/protobuf v1.30.0 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- main.go
+++ main.go
@@ -1,20 +1,25 @@
 package main
 
 import (
+	"bytes"
 	"encoding/binary"
 	"encoding/hex"
 	"fmt"
 	"log"
 	"os"
 	"strconv"
+	"strings"
 
 	"github.com/pkg/errors"
+	"github.com/pokt-network/pocket-core/app"
 	"github.com/pokt-network/pocket-core/codec"
 	"github.com/pokt-network/pocket-core/codec/types"
 	"github.com/pokt-network/pocket-core/crypto"
 	"github.com/pokt-network/pocket-core/store/rootmulti"
+	nodeTypes "github.com/pokt-network/pocket-core/x/nodes/types"
 	pcTypes "github.com/pokt-network/pocket-core/x/pocketcore/types"
 	"github.com/syndtr/goleveldb/leveldb"
+	"github.com/syndtr/goleveldb/leveldb/util"
 	"github.com/tendermint/go-amino"
 )
 
@@ -54,56 +59,312 @@ func getLatestHeight(db *leveldb.DB) int64 {
 	return latest
 }
 
-func dumpCommitInfo(db *leveldb.DB, height int64) {
+func getChildrenFromNode(buf []byte) (
+	version int64,
+	leftHash, rightHash []byte,
+	key, value []byte,
+	err error) {
+	height, n, cause := amino.DecodeInt8(buf)
+	if cause != nil {
+		err = errors.Wrap(cause, "decoding node.height")
+		return
+	}
+
+	buf = buf[n:]
+
+	_, n, cause = amino.DecodeVarint(buf)
+	if cause != nil {
+		err = errors.Wrap(cause, "decoding node.size")
+		return
+	}
+	buf = buf[n:]
+
+	version, n, cause = amino.DecodeVarint(buf)
+	if cause != nil {
+		err = errors.Wrap(cause, "decoding node.version")
+		return
+	}
+	buf = buf[n:]
+
+	key, n, cause = amino.DecodeByteSlice(buf)
+	if cause != nil {
+		err = errors.Wrap(cause, "decoding node.key")
+		return
+	}
+	buf = buf[n:]
+
+	if height == 0 {
+		// this node is a leaf
+		value, _, cause = amino.DecodeByteSlice(buf)
+		if cause != nil {
+			err = errors.Wrap(cause, "decoding node.value")
+			return
+		}
+		return
+	}
+
+	leftHash, n, cause = amino.DecodeByteSlice(buf)
+	if cause != nil {
+		err = errors.Wrap(cause, "decoding node.leftHash")
+		return
+	}
+	buf = buf[n:]
+
+	rightHash, _, cause = amino.DecodeByteSlice(buf)
+	if cause != nil {
+		err = errors.Wrap(cause, "decoding node.rightHash")
+		return
+	}
+
+	return
+}
+
+var (
+	AllValidatorsKey = []byte{0x21}
+)
+
+func parseNode(key, value []byte) (res string) {
+	if bytes.HasPrefix(key, AllValidatorsKey) {
+		var valNew nodeTypes.Validator
+		err := app.Codec().UnmarshalBinaryLengthPrefixed(value, &valNew, 0)
+		if err == nil {
+			return "Val " + valNew.Address.String()
+		}
+
+		var valLegacy nodeTypes.LegacyValidator
+		err = app.Codec().UnmarshalBinaryLengthPrefixed(value, &valLegacy, 0)
+		if err == nil {
+			return "LVal " + valNew.Address.String()
+		}
+
+		return "err:" + err.Error()
+	}
+
+	return
+}
+
+func dfs(db *leveldb.DB, prefix, hash []byte, depth int) {
+	if len(hash) == 0 {
+		return
+	}
+
+	nodeKey := make([]byte, len(prefix), len(prefix)+1+len(hash))
+	copy(nodeKey, prefix)
+	nodeKey = append(nodeKey, 'n')
+	nodeKey = append(nodeKey, hash...)
+
+	nodeValue, err := db.Get(nodeKey, nil)
+	if err != nil {
+		log.Fatalf("Not found: %x", nodeKey)
+		return
+	}
+
+	ver, l, r, k, v, err := getChildrenFromNode(nodeValue)
+	if err != nil {
+		log.Fatalf("%s: %s", err.Error(), nodeKey)
+		return
+	}
+
+	var nodeType rune
+	if len(l) == 0 {
+		if len(r) == 0 {
+			nodeType = 'l'
+		} else {
+			nodeType = '!'
+		}
+	} else if len(r) == 0 {
+		nodeType = '!'
+	} else {
+		nodeType = 'n'
+	}
+
+	kv := ""
+	if nodeType == 'l' {
+		if bytes.Contains(prefix, []byte("k:params")) {
+			kv = fmt.Sprintf("%v -> %v", string(k), string(v))
+		} else if bytes.Contains(prefix, []byte("k:pos")) {
+			kv = parseNode(k, v)
+		}
+	}
+
+	fmt.Printf("%s%x %c %v %v\n",
+		strings.Repeat(" ", depth),
+		hash,
+		nodeType,
+		ver,
+		kv)
+
+	dfs(db, prefix, l, depth+1)
+	dfs(db, prefix, r, depth+1)
+}
+
+func getBlockRange(blockRange string, latest int64) []int64 {
+	safeParse := func(str string, def int64) int64 {
+		if len(str) == 0 {
+			return def
+		}
+		if n, err := strconv.ParseInt(str, 10, 64); err == nil {
+			return n
+		}
+		return int64(0)
+	}
+
+	tokens := strings.Split(blockRange, ":")
+	if len(tokens) == 2 {
+		a := safeParse(tokens[0], latest)
+		if a <= 0 {
+			log.Fatalln("invalid token:", tokens[0])
+		}
+		b := safeParse(tokens[1], latest)
+		if b <= 0 {
+			log.Fatalln("invalid token:", tokens[1])
+		}
+
+		seq := make([]int64, b-a+1)
+		for i := 0; i < len(seq); i++ {
+			seq[i] = a + int64(i)
+		}
+		return seq
+	}
+
+	tokens = strings.Split(blockRange, ",")
+	if len(tokens) == 1 {
+		a, err := strconv.ParseInt(tokens[0], 10, 64)
+		if err != nil || a == 0 {
+			log.Fatalln("invalid token:", tokens[0])
+		}
+		if a < 0 {
+			seq := make([]int64, -a)
+			for i := int64(0); i < -a; i++ {
+				seq[i] = latest - i
+			}
+			return seq
+		}
+	}
+
+	seq := make([]int64, len(tokens))
+	for i := 0; i < len(seq); i++ {
+		a, err := strconv.ParseInt(tokens[i], 10, 64)
+		if err != nil {
+			log.Fatalln("invalid token:", tokens[i])
+		}
+		seq[i] = a
+	}
+	return seq
+}
+
+func dumpOrphan(db *leveldb.DB, store string) {
+	prefix := "s/k:" + store + "/o"
+	it := db.NewIterator(util.BytesPrefix([]byte(prefix)), nil)
+	for it.Next() {
+		orphanKey := it.Key()
+		orphanValue := it.Value()
+
+		prefixLen := len(prefix)
+		if len(orphanKey) != prefixLen+8+8+len(orphanValue) {
+			fmt.Println("Unexpected length at: ", hex.EncodeToString(orphanKey))
+			continue
+		}
+		if !bytes.HasSuffix(orphanKey, orphanValue) {
+			fmt.Println("Unexpected value at: ", hex.EncodeToString(orphanKey))
+			continue
+		}
+
+		verTo := binary.BigEndian.Uint64(orphanKey[prefixLen : prefixLen+8])
+		prefixLen = prefixLen + 8
+		verFrom := binary.BigEndian.Uint64(orphanKey[prefixLen : prefixLen+8])
+		fmt.Printf("%s [%v-%v] %s\n",
+			prefix, verFrom, verTo, hex.EncodeToString(orphanValue))
+	}
+}
+
+func dumpCommitInfo(db *leveldb.DB, heights []int64) {
 	cdc := codec.NewCodec(types.NewInterfaceRegistry())
 	pcTypes.RegisterCodec(cdc)
 	crypto.RegisterAmino(cdc.AminoCodec().Amino)
 
-	ciBytes, err := db.Get([]byte(fmt.Sprintf("s/%d", height)), nil)
-	if err != nil {
-		fmt.Println("Unexpected commitInfo at ", height)
-		return
-	}
+	for _, height := range heights {
+		ciBytes, err := db.Get([]byte(fmt.Sprintf("s/%d", height)), nil)
+		if err != nil {
+			fmt.Println("Unexpected commitInfo at ", height)
+			continue
+		}
 
-	var cInfo rootmulti.CommitInfo
-	err = cdc.LegacyUnmarshalBinaryLengthPrefixed(ciBytes, &cInfo)
-	if err != nil {
-		fmt.Println("Failed to unmarhsal commitInfo at ", height)
-		return
-	}
+		var cInfo rootmulti.CommitInfo
+		err = cdc.LegacyUnmarshalBinaryLengthPrefixed(ciBytes, &cInfo)
+		if err != nil {
+			fmt.Println("Failed to unmarhsal commitInfo at ", height)
+			continue
+		}
 
-	fmt.Printf("%d: %x\n", cInfo.Version, cInfo.Hash())
-	for _, store := range cInfo.StoreInfos {
-		fmt.Printf(
-			"%12s:%v %v\n",
-			store.Name,
-			store.Core.CommitID.Version,
-			hex.EncodeToString(store.Core.CommitID.Hash),
-		)
+		fmt.Println("Version:", cInfo.Version)
+		for _, store := range cInfo.StoreInfos {
+			fmt.Printf(
+				"%12s:%v %v\n",
+				store.Name,
+				store.Core.CommitID.Version,
+				hex.EncodeToString(store.Core.CommitID.Hash),
+			)
+		}
 	}
 }
 
 func main() {
-	var appDbDir string
-	if len(os.Args) >= 2 {
-		appDbDir = os.Args[1]
-	} else {
-		appDbDir = os.ExpandEnv("$HOME/.pocket/data/application.db")
+	if len(os.Args) < 4 {
+		fmt.Print(`USAGE:
+dump <appdb> <store> <range>
+dump <appdb> ci <range>
+dump <appdb> !<store> <hash>
+
+range:
+  100
+	1,100
+	1:100
+	:1
+`)
+		return
 	}
 
-	var height int64
-	if len(os.Args) >= 3 {
-		height, _ = strconv.ParseInt(os.Args[2], 10, 64)
-	}
+	dir := os.Args[1]
+	store := os.Args[2]
+	blockrange := os.Args[3]
 
-	srcDb, err := leveldb.OpenFile(appDbDir, nil)
+	srcDb, err := leveldb.OpenFile(dir, nil)
 	if err != nil {
-		log.Fatal("Failed to open db: " + appDbDir)
+		log.Fatal("Failed to open db: " + dir)
 	}
-	defer srcDb.Close()
 
-	if height == 0 {
-		height = getLatestHeight(srcDb)
+	if strings.HasPrefix(store, "!") {
+		// hash, err := hex.DecodeString(blockrange)
+		// if err != nil {
+		// 	log.Fatal("Invalid hash: " + blockrange)
+		// }
+		dumpOrphan(srcDb, store[1:])
+		return
 	}
-	dumpCommitInfo(srcDb, height)
+
+	latestHeight := getLatestHeight(srcDb)
+	heights := getBlockRange(blockrange, latestHeight)
+
+	if store == "ci" {
+		dumpCommitInfo(srcDb, heights)
+		return
+	}
+
+	for _, height := range heights {
+		prefix := []byte("s/k:" + store + "/")
+		fmt.Printf("# %s%v\n", string(prefix), height)
+
+		key := make([]byte, 0, len(prefix)+1+8)
+		key = append(key, prefix...)
+		key = append(key, 'r')
+		key = binary.BigEndian.AppendUint64(key, uint64(height))
+
+		hash, err := srcDb.Get(key, nil)
+		if err != nil {
+			log.Fatal("Failed to get data")
+		}
+
+		dfs(srcDb, prefix, hash, 0)
+	}
 }
